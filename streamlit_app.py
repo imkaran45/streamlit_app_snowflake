4b60ccac8afbe9fb2a663f17df7495cc74356faa
imkaran45 <murjanikaran45@gmail.com>
2023-06-30 15:39:41 +1000

    Update streamlit_app.py
    adding try and except

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,15 +25,28 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?")
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas. json_normalize(fruityvice_response.json ())
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  streamlit.error ()
+
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# streamlit.write('The user entered ', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-#Normalise function from pandas to show it in dataframe
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized) #Print dataframe
+# #Normalise function from pandas to show it in dataframe
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# streamlit.dataframe(fruityvice_normalized) #Print dataframe
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
